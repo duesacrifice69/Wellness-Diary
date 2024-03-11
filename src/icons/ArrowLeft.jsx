34ce373dfae0508110026b154619ed9665d5700b
duesacrifice69 <chirathtimodh@gmail.com>
2024-03-11 21:10:04 +0530

    icons added

---
+++ src/icons/ArrowLeft.jsx
@@ -0,0 +1,19 @@
+export default function ArrowLeft() {
+  return (
+    <svg
+      width="13"
+      height="13"
+      viewBox="0 0 13 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.61536 11.5L0.99998 6.5M0.99998 6.5L5.61536 1.5M0.99998 6.5H11.8333"
+        stroke="#1F2B6C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
